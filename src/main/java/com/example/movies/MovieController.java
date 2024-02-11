cea6dd11df2796ebb69f668eb48510e0b1d773e0
janath <janathhma@gmail.com>
2024-02-11 05:45:31 +0530

    Implement the 'find movie by ID' endpoint.

--- src/main/java/com/example/movies/MovieController.java
+++ src/main/java/com/example/movies/MovieController.java
@@ -1,14 +1,17 @@
 package com.example.movies;
 
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -18,5 +21,10 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllmovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id),HttpStatus.OK);
     }
 }
--- src/main/java/com/example/movies/MovieService.java
+++ src/main/java/com/example/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.example.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -11,5 +13,8 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 }
